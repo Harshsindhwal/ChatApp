c739c9a9ec23c8a5bd9c163d6ab72579269667ff
Harsh <harshsindhwal2018820@gmail.com>
2024-09-10 23:06:19 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@
 import express from "express";
 import dotenv from "dotenv"; 
 import authRoutes from "./routes/auth.routes.js"
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
 const app = express();
 
 dotenv.config();
@@ -14,4 +16,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth",authRoutes)
 
-app.listen(PORT, () => console.log(`Server is Running on port ${PORT}`));
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is Running on port ${PORT}`);
+});
