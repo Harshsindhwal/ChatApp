efb531058ec281c538e5d2f3de8541165164e175
Harsh <harshsindhwal2018820@gmail.com>
2024-10-30 13:16:24 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-//const express = require("express");
 import express from "express";
 import dotenv from "dotenv"; 
-import cookieParser from "cookie-parser"; 
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -17,6 +19,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     connectToMongoDB();
