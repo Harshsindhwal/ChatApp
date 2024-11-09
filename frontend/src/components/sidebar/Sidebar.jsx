888fa6519fb08f3d589f81775e4afa2b75246c06
Harsh <harshsindhwal2018820@gmail.com>
2024-11-09 21:55:58 +0530

    commit

---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import SearchInput from "./SearchInput";
+
+const Sidebar = () => {
+    return (
+        <div>
+            <SearchInput />
+            <div className='divider px-3'></div>
+        </div>
+    )
+}
+export default Sidebar;
