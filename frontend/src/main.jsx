39bbbab2211a8485aa50e1b92c6a7a8cdb58b161
Harsh <harshsindhwal2018820@gmail.com>
2024-11-14 22:24:15 +0530

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthContextProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
